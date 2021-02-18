406e44e48c849a290d39d128e9a8aa8e42e86775
lzmlzm <1198492751@qq.com>
2021-02-18 20:27:42 +0800

    fix DirectLight sample

--- app/src/main/java/com/muglzm/opengl/render/lighting/DirectionLightRender.kt
+++ app/src/main/java/com/muglzm/opengl/render/lighting/DirectionLightRender.kt
@@ -3,7 +3,7 @@ package com.muglzm.opengl.render.lighting
 import android.opengl.GLES30
 import javax.microedition.khronos.opengles.GL10
 
-class DirectionLightRender :LightingRenderer("lighting/directionallight.vs", "lighting/directionallight.fs"){
+class DirectionLightRender :LightingRenderer("lighting/directlight_vertex.glsl", "lighting/directlight_fragment.glsl"){
     override fun onDrawFrame(gl: GL10?) {
         super.onDrawFrame(gl)
         GLES30.glUniform3f(GLES30.glGetUniformLocation(programId, "lightDirection"), -5f, 0f, 0f)
